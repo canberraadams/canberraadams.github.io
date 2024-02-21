9e28740ec3558af0f75b0759a13f2334aa3206d5
canberraadams <159619345+canberraadams@users.noreply.github.com>
2024-02-21 11:31:29 +1100

    Update and rename nav.html to nav.js

--- nav.js
+++ nav.js
@@ -1,6 +1,8 @@
+document.write(`
   <nav>
     <a href="index.html">Home</a>
     <a href="about.html">About</a>
     <a href="services.html">Services</a>
     <a href="#">Contact</a>
   </nav>
+`);
